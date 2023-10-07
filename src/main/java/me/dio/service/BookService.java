6de9839e5445d5bd9c2a445b9b418489abd45c93
Chardaviski <eduardaleal753@gmail.com>
2023-10-07 12:52:12 -0300

    Swagger não reconhece

---
+++ src/main/java/me/dio/service/BookService.java
@@ -0,0 +1,9 @@
+package me.dio.service;
+
+import me.dio.domain.model.Book;
+
+public interface BookService {
+    Book findById(Long id);
+
+    Book create(Book bookToCreate);
+}
---
+++ src/main/java/me/dio/service/impl/BookServiceImpl.java
@@ -0,0 +1,30 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.Book;
+import me.dio.domain.repository.BookRepository;
+import me.dio.service.BookService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+    public BookServiceImpl(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book findById(Long id) {
+        return bookRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public Book create(Book bookToCreate) {
+        if (bookToCreate.getId() != null && bookRepository.existsById(bookToCreate.getId())){
+            throw new IllegalArgumentException("This user ID already exists.");
+        }
+        return bookRepository.save(bookToCreate);
+    }
+}
